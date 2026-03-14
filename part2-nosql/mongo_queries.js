63dc21b32631ff669cfe42d3bd5cf117c4db5c76
Richajain02 <richajain56789@gmail.com>
2026-03-14 18:17:53 +0530

    Update mongo_queries.js

--- part2-nosql/mongo_queries.js
+++ part2-nosql/mongo_queries.js
@@ -59,22 +59,23 @@ db.products.insertMany([
 db.products.find({
   category: "Electronics",
   price: { $gt: 20000 }
-})
+});
 
 // OP3: find() — retrieve all Groceries expiring before 2025-01-01
 
 db.products.find({
   category: "Groceries",
   expiry_date: { $lt: new Date("2025-01-01") }
-})
+});
+
 
 // OP4: updateOne() — add a "discount_percent" field to a specific product
 
 db.products.updateOne(
   { product_name: "Wireless Headphones" },
   { $set: { discount_percent: 10 } }
-)
+);
 
 // OP5: createIndex() — create an index on category field
 
-db.products.createIndex({ category: 1 })
+db.products.createIndex({ category: 1 });
